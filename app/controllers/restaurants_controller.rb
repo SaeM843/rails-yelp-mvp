beb4fc26b71b90163990af761b4caaff3d3d7bff
Sae Matsuda <sae.matsuda.843@gmail.com>
2022-08-10 16:10:43 +0900

    Restaurants controller added

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurnts.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render
+  end
+end
